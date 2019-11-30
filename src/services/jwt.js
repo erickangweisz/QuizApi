d7421e1450458eef354850e7a55cca79a0928c67
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-11-30 17:13:13 +0000

    implement decodetoken function and export it

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -2,12 +2,33 @@ const jwt = require('jwt-simple')
 const moment = require('moment')
 const config = require('../config')
 
-exports.createToken = function(user) {  
+exports.createToken = (user) => {  
   let payload = {
     sub: user._id,
     iat: moment().unix(),
     exp: moment().add(14, "days").unix(),
   }
-
   return jwt.encode(payload, config.privateKey)
+}
+
+exports.decodeToken(token) = () => {
+  const decoded = new Promise((resolve, reject) => {
+      try {
+          const payload = jwt.decode(token, config.privateKey)
+
+          if (payload.exp <= moment().unix()) {
+              reject({
+                  status: 401,
+                  message: 'the token has expired'
+              })
+          }
+          resolve(payload.sub)
+      } catch (err) {
+          reject({
+              status: 500,
+              message: 'invalid token'
+          })
+      }
+  })
+  return decoded
 }
