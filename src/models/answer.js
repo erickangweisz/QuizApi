08749e453202a2c4bfc640b8dc15d9d5ad0018c9
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-11-30 21:47:50 +0000

    rename idpublishing to idpublication property in answer model

--- src/models/answer.js
+++ src/models/answer.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const AnswerSchema = new Schema({
     description: { type: String, required: true },
     numVotes: { type: number, default: 0 },
-    idPublishing: { type: String, required: true }
+    idPublication: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Answer', AnswerSchema)
