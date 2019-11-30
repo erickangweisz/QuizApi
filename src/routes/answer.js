1f233a092268bdbb48d80e339abbd88dbeac1d9b
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-11-30 17:35:24 +0000

    add middleware ensureAuth function in answer router to protect paths that require token

--- src/routes/answer.js
+++ src/routes/answer.js
@@ -1,10 +1,22 @@
 const Router = require('express')
 const answerController = require('../controllers/answer')
+const auth = require('../middlewares/authenticated')
 
 const router = new Router()
 
-router.post('/answer', answerController.createAnswer)
-router.get('/answers/:publicationId', answerController.getAnswerByPublicationId)
-router.delete('/answer/:answerId', answerController.deleteAnswer)
+router.post(
+    '/answer', 
+    auth.ensureAuth, 
+    answerController.createAnswer
+)
+router.get(
+    '/answers/:publicationId', 
+    answerController.getAnswerByPublicationId
+)
+router.delete(
+    '/answer/:answerId', 
+    auth.ensureAuth, 
+    answerController.deleteAnswer
+)
 
 module.exports = router
