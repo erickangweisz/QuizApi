d8e64babf285fd3ee807b1663ee9e94a9080cb37
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-11-30 15:24:20 +0000

    add register and login function in auth controller with authValidator middleware

---
+++ src/controllers/auth.js
@@ -0,0 +1,43 @@
+const jwt = require('../services/jwt')
+const User = require('../models/user')
+const authValidator = require('../middlewares/auth_validator')
+
+function register(req, res) {
+    const user = new User({
+        email: req.body._email,
+        username: req.body._username,
+        role: req.body._role
+    })
+
+    authValidator.registerValidator(req, res, () => {
+        let passwordHashed = res.locals.hash
+        user.password = passwordHashed
+
+        user.save((saveErr, savedUser) => {
+            if (saveErr)
+                res.status(500).send({ message: `Error: ${saveErr}` })
+            
+            res.status(201).send({ token: jwt.createToken(savedUser) })
+        })
+    })
+}
+
+function login(req, res) {
+    authValidator.loginValidator(req, res, () => {
+        const email = req.body['email'].toLowerCase().trim()
+
+        User.findOne({ email: email.toLowerCase().trim() }, (loginErr, user) => {
+            if (loginErr)
+                res.status(500).send({ message: `Error: ${loginErr}` })
+            
+            res.status(200).send({
+                token: jwt.createToken(user)
+            })
+        })
+    })
+}
+
+module.exports = {
+    register,
+    login
+}
