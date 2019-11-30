e4ad1a1a91e69797c7abe4f661ef5b49c86af388
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-11-30 15:46:37 +0000

    implement CRUD functions in user controller pending of validate

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -13,6 +13,50 @@ function getUsers(req, res) {
     })
 }
 
+function updateUser(req, res) {
+    const userId = req.params.userId
+    const update = req.body
+
+    User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+        if (err) 
+            res.status(500).send({ message: `error updating user: ${err}` })
+
+        res.status(200).send({ user: userUpdated })
+    })
+}
+
+function getUserById(req, res) {
+    let userId = req.params.userId
+
+    User.findOneAndUpdate({ _id: userId }, (err, user) => {
+        if (err) 
+            return res.status(500).send({ message: `request error: ${err}` })
+        if (!user) 
+            return res.status(404).send({ message: 'the user dont exist' })
+
+        res.status(200).send({ user })
+    })
+}
+
+function deleteUser(req, res) {
+    let userId = req.params.userId
+
+    User.findById(userId, (err, user) => {
+        if (err) 
+            res.status(500).send({ message: `error deleting user: ${err}` })
+
+        user.remove(err => {
+            if (err) 
+                res.status(500).send({ message: `error deleting user: ${err}` })
+            
+            res.status(200).send({ message: 'the user has been deleted' })
+        })
+    })
+}
+
 module.exports = {
-    getUsers
+    getUsers,
+    updateUser,
+    getUserById,
+    deleteUser
 }
