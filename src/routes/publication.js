4ac2027232a9e794cb0b01927c1efd2f08d8fcdb
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-11-30 16:54:36 +0000

    call all functions from controller in publication routes file

--- src/routes/publication.js
+++ src/routes/publication.js
@@ -3,6 +3,10 @@ const publicationController = require('../controllers/publication')
 
 const router = new Router()
 
+router.post('/publication', publicationController.createPublication)
 router.get('/publications', publicationController.getPublications)
+router.get('/publication/:publicationId', publicationController.getPublicationById)
+router.put('/publication/:publicationId', publicationController.updatePublication)
+router.delete('/publication/:publicationId', publicationController.deletePublication)
 
 module.exports = router
